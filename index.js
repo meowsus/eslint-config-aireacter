248a0c79ef70bb2f168cd2d017094d1948d0eb7e
Curt Howard <curt@portugly.com>
2021-11-24 09:58:21 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ["airbnb", "plugin:prettier/recommended", "prettier"],
+
+  plugins: ["prettier"],
+
+  rules: {
+    "prettier/prettier": "error",
+  },
+
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+};
